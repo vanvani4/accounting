5e16deab90d6ceecddbd291e0b3ec00e1a676ed4
Іорданов Іван Іванович <@port.odessa.ua>
2020-04-23 14:34:30 +0300

    Added 'About us' and 'Footer' layouts

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,17 +2,23 @@ import { NgModule } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   imports: [
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTabsModule,
+    MatIconModule
   ],
   exports: [
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTabsModule,
+    MatIconModule
   ]
 })
 export class MaterialModule {}
